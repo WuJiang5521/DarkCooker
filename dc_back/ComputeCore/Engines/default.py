23923f91a2dcbefd654dbac8900b1b131050cac8
Wu Jiang <wujiang5521@qq.com>
2019-01-05 21:56:21 +0800

    update

--- dc_back/ComputeCore/Engines/default.py
+++ dc_back/ComputeCore/Engines/default.py
@@ -7,11 +7,6 @@ class DefaultEngine(Engine):
         root = Node()
         root.situations = [i for i in range(len(self.model['situations']))]
 
-        child = Node()
-        child.pos = (1, 1)
-        child.meet = 0
-        child.situations = [i for i in range(len(self.model['situations']))]
-        child.father = root
-        root.children[4].append(child)
+
 
         return root.transform()
